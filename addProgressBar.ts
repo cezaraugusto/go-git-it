1c4ad29c86ec4cb63031be58c1906297ce419947
Cezar Augusto <boss@cezaraugusto.net>
2024-02-20 11:49:55 -0300

    General improvements:
    Fix #2
    
    - Improve async handling
    - Add progress bar
    - Add custom text message option

---
+++ addProgressBar.ts
@@ -0,0 +1,48 @@
+// @ts-ignore
+import ProgressBar from 'progress'
+
+export default function addProgressBar(
+  text: string,
+  completionCallback: () => Promise<void>
+): Promise<void> {
+  return new Promise<void>(async (resolve, reject) => {
+    const contentLength = 2048 * 1024 // 2MB
+    const bar = new ProgressBar(`${text} [:bar] :percent :etas`, {
+      complete: '=',
+      incomplete: ' ',
+      width: 50,
+      total: contentLength
+    })
+
+    // Simulate progress
+    let progress = 0
+
+    const timer = setInterval(() => {
+      const chunk = Math.random() * 10 * 1024
+      progress += chunk
+      bar.tick(chunk)
+      if (progress >= contentLength || bar.complete) {
+        // Clear interval when progress is complete
+        clearInterval(timer)
+      }
+    }, 50)
+
+    try {
+      // Wait for the cloning operation to complete
+      await completionCallback()
+
+      // Ensure the interval is cleared after operation completes
+      clearInterval(timer)
+
+      // Force-complete the progress bar
+      bar.tick(contentLength)
+      resolve()
+    } catch (error) {
+      // Ensure the interval is cleared in case of error
+      clearInterval(timer)
+
+      console.error('[go-git-it] An error occurred:', error)
+      reject(error)
+    }
+  })
+}
--- module.test.js
+++ module.test.js
@@ -7,32 +7,27 @@ const goGitIt = require('./dist/module').default
 
 const repoURL = 'https://github.com/cezaraugusto/extension-create'
 const folderURL =
-  'https://github.com/cezaraugusto/extension-create/tree/main/create'
+  'https://github.com/cezaraugusto/extension-create/tree/main/packages'
 const fileURL =
-  'https://github.com/cezaraugusto/extension-create/tree/main/create/cli.js'
+  'https://github.com/cezaraugusto/extension-create/tree/main/package.json'
 const customPath = path.resolve(__dirname, 'some/extraordinary/folder')
 
-// Disable console.logs as we don't care about them
-for (const func in console) {
-  console[func] = function () {}
-}
-
 describe('go-git-it', () => {
-  describe('working with directories', () => {
+  describe('working with full URLs', () => {
     afterEach(() => {
       shell.rm('-rf', path.resolve(__dirname, path.basename(repoURL)))
       shell.rm('-rf', path.resolve(__dirname, 'some'))
     })
-    test('works with repositories', async () => {
-      goGitIt(repoURL)
+    test('works with default path', async () => {
+      await goGitIt(repoURL)
 
       const pathName = path.resolve(__dirname, path.basename(repoURL))
 
       expect(await fs.pathExists(pathName)).toBe(true)
     })
 
-    test('using a custom path works with repositories', async () => {
-      goGitIt(repoURL, customPath)
+    test('works with a custom path', async () => {
+      await goGitIt(repoURL, customPath)
 
       const pathName = path.resolve(customPath, path.basename(repoURL))
 
@@ -40,47 +35,47 @@ describe('go-git-it', () => {
     })
   })
 
-  describe('files', () => {
+  describe('working with partial URLs (basename is file)', () => {
     afterEach(() => {
       shell.rm('-rf', path.basename(fileURL))
       shell.rm('-rf', path.resolve(__dirname, 'some'))
     })
 
-    test('works with files', async () => {
-      goGitIt(fileURL)
+    test('works with default path', async () => {
+      await goGitIt(fileURL)
 
-      const pathName = path.resolve(process.cwd(), 'cli.js')
+      const pathName = path.resolve(process.cwd(), 'package.json')
 
       expect(await fs.pathExists(pathName)).toBe(true)
     })
 
-    test('using a custom path works with files', async () => {
-      goGitIt(fileURL, customPath)
+    test('works with a custom path', async () => {
+      await goGitIt(fileURL, customPath)
 
-      const pathName = path.resolve(customPath, 'cli.js')
+      const pathName = path.resolve(customPath, 'package.json')
 
       expect(await fs.pathExists(pathName)).toBe(true)
     })
   })
 
-  describe('folder', () => {
+  describe('working with partial URLs (basename is folder)', () => {
     afterEach(() => {
-      shell.rm('-rf', path.resolve(__dirname, 'create'))
+      shell.rm('-rf', path.resolve(__dirname, 'packages'))
       shell.rm('-rf', path.resolve(__dirname, 'some'))
     })
 
-    test('works with folders', async () => {
-      goGitIt(folderURL)
+    test('works with default path', async () => {
+      await goGitIt(folderURL)
 
-      const pathName = path.resolve(process.cwd(), 'create')
+      const pathName = path.resolve(process.cwd(), 'packages')
 
       expect(await fs.pathExists(pathName)).toBe(true)
     })
 
-    test('using a custom path works with folders', async () => {
-      goGitIt(folderURL, customPath)
+    test('works with a custom path', async () => {
+      await goGitIt(folderURL, customPath)
 
-      const pathName = path.resolve(customPath, 'create')
+      const pathName = path.resolve(customPath, 'packages')
 
       expect(await fs.pathExists(pathName)).toBe(true)
     })
